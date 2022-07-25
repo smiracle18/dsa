16058d6fd72afd4d36c88aa22f8c07c829a27ddc
smiracle18 <serenesushant@gmail.com>
2022-07-25 18:04:28 +0530

    fixed typo and added test cases for function

--- Searching/binary_search.py
+++ Searching/binary_search.py
@@ -1,19 +1,23 @@
-def binary_search(arr, target):
-    ''' input: sorted arr and returns the occurance of target in it.
-        output: index of target in arr starting from 0.
+"""
+Binary Search Module
+"""
+from multiprocessing.dummy import Array
+
+
+def binary_search(arr: Array, target: int) -> int:
+    ''' 
+    input: sorted arr and returns the occurance of target in it.
+    output: index of target in arr starting from 0.
     '''
     low, high = 0, len(arr) - 1
-
-    while low <= high:
+    while low <= high:       
         mid = (low + high) // 2
-
         if arr[mid] == target:
             return mid
         
         if arr[mid] < target:
-            l = mid + 1
+            low= mid + 1
         else:
-            r = mid - 1
+            high = mid - 1
     return -1
 
-print(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5))
