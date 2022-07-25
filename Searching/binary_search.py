a7eaec5dbe192cb9acc04a0171e02fd6825476be
smiracle18 <serenesushant@gmail.com>
2022-07-25 18:24:04 +0530

    add first occurance function

--- Searching/binary_search.py
+++ Searching/binary_search.py
@@ -1,23 +1,43 @@
 """
-Binary Search Module
+BinarySearch Module
 """
 from multiprocessing.dummy import Array
 
 
-def binary_search(arr: Array, target: int) -> int:
-    ''' 
-    input: sorted arr and returns the occurance of target in it.
-    output: index of target in arr starting from 0.
-    '''
-    low, high = 0, len(arr) - 1
-    while low <= high:       
-        mid = (low + high) // 2
-        if arr[mid] == target:
-            return mid
-        
-        if arr[mid] < target:
-            low= mid + 1
-        else:
-            high = mid - 1
-    return -1
+class BinarySearch:
+    
+    def binary_search(arr: Array, target: int) -> int:
+        ''' 
+        input: sorted arr and returns the occurance of target in it.
+        output: index of target in arr starting from 0.
+        '''
+        low, high = 0, len(arr) - 1
+        while low <= high:       
+            mid = (low + high) // 2
+            if arr[mid] == target:
+                return mid
+            
+            if arr[mid] < target:
+                low= mid + 1
+            else:
+                high = mid - 1
+        return -1
 
+    def binary_search_left(arr: Array, target: int) -> int:
+        ''' 
+        input: sorted arr and returns the first occurance of target in it.
+        output: index of target in arr starting from 0.
+        '''
+        low, high = 0, len(arr) - 1
+        found_at = -1
+        while low <= high:       
+            mid = (low + high) // 2
+            if arr[mid] == target:
+                found_at = mid
+                high = mid - 1
+
+            elif arr[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return found_at
--- Searching/test_binary_search.py
+++ Searching/test_binary_search.py
@@ -2,7 +2,7 @@
 Test module for binary_search.py
 """
 import unittest
-from binary_search import binary_search
+from binary_search import BinarySearch
 
 
 class TestBinarySearch(unittest.TestCase):
@@ -16,15 +16,50 @@ class TestBinarySearch(unittest.TestCase):
         """
         arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         target = 5
-        self.assertEqual(binary_search(arr, target), 4)
+        self.assertEqual(BinarySearch.binary_search(arr, target), 4)
 
     def test_binary_search_with_duplicates(self):
         """
         Test binary_search() function
         """
         arr, target = [1, 2, 2, 2, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 10], 5
-        self.assertIn(binary_search(arr, target), [7, 8, 9])
+        self.assertIn(BinarySearch.binary_search(arr, target), [7, 8, 9])
 
+    def test_binary_search_without_target(self):
+        """
+        Test binary_search() function
+        """
+        arr, target = [1, 2, 2, 2, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 10], 12
+        self.assertIn(BinarySearch.binary_search(arr, target), [-1])
+
+
+        
+
+    '''Left test cases'''
+
+    def test_binary_search_left(self):
+        """
+        Test binary_search_left() function
+        """
+        arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        target = 5
+        self.assertEqual(BinarySearch.binary_search_left(arr, target), 4)
+
+    def test_binary_search_left_with_duplicates(self):
+        """
+        Test binary_search_left() function
+        """
+        arr, target = [1, 2, 2, 2, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 10], 5
+        self.assertIn(BinarySearch.binary_search_left(arr, target), [7])
+
+    def test_binary_search_left_without_target(self):
+        """
+        Test binary_search_left() function
+        """
+        arr, target = [1, 2, 2, 2, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 10], 12
+        self.assertIn(BinarySearch.binary_search_left(arr, target), [-1])
+
+    
 
 if __name__ == '__main__':
     unittest.main()
